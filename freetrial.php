ec8e7bc64fe3a84a5f384c22b08a2b0d8a42f63c
Suparna Chowdhury <kamela@metrodesk.com.bd>
2016-11-09 13:44:18 +0600

    Add files via upload

---
+++ freetrial.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+ * Plugin Name: ImageEditing Free Trial
+ * Plugin URI: 
+ * Description: This plugin adds a free tiral form which allows user to upload images and submit to try services provided. The image uploading and handling is done using Uploadcare. Enjoy this plugin! 
+ * Version: 1.0.0
+ * Author: Kamela Chowdhury
+ * Author URI: peppylish.com
+ * License: GPL2
+ */
+?>
