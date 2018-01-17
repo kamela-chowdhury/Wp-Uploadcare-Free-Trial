90118f8764c29261ce2221ea8467e8ccfc2f14b9
Suparna Chowdhury <kamela@metrodesk.com.bd>
2018-01-17 11:44:50 +0600

    New features
    added country list

--- freetrial.php
+++ freetrial.php
@@ -1,120 +1,123 @@
 <?php 
 /**
- * Plugin Name: ImageEditing Free Trial
- * Plugin URI: https://github.com/kamela-peppylish/ImageEditing-Free-Trial
+ * Plugin Name: Free Trial
+ * Plugin URI: https://github.com/kamela-peppylish/Wp-Uploadcare-Free-Trial
  * Description: This plugin adds a free trial form which allows user to upload images and submit to try services provided. The image uploading and handling is done using Uploadcare. Enjoy this plugin! 
- * Version: 1.0.0
+ * Version: 2.0.0
  * Author: Kamela Chowdhury
- * Author URI: peppylish.com
+ * Author URI: https://about.me/kamelac
  * License: GPL2
  */
 require_once 'vendor/autoload.php';
+include 'country-list.php';
+function get_client_ip() {
+     $ipaddress = '';
+     if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+     else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+     else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+     else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+     else if(getenv('HTTP_FORWARDED'))
+        $ipaddress = getenv('HTTP_FORWARDED');
+     else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+     else
+        $ipaddress = 'UNKNOWN';
+     return $ipaddress;
+}
 
+function ip_details($url) {
+   $ch = curl_init();
+   curl_setopt($ch, CURLOPT_URL, $url);
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+   $data = curl_exec($ch);
+   curl_close($ch);
+
+   return $data;
+}
 function trial_form(){
 ?>
-<script src="https://ucarecdn.com/widget/2.10.0/uploadcare/uploadcare.full.min.js" charset="utf-8">
+<script src="https://ucarecdn.com/widget/2.10.0/uploadcare/uploadcare.full.min.js" charset="utf-8" defer></script>
+<script type="text/javascript" src="<?php echo plugin_dir_url(__FILE__);?>ie-uploadcare.js" defer></script>
+<link rel="stylesheet" type="text/css" href="<?php echo plugin_dir_url(__FILE__);?>trial-styles.css">
+<script type="text/javascript">
+  UPLOADCARE_LOCALE = "en";
+  UPLOADCARE_TABS = "file";
+  UPLOADCARE_PUBLIC_KEY = "your uploadcare public key";
 </script>
-    <script>
-      // Widget settings
-      UPLOADCARE_LOCALE = "en";
-      UPLOADCARE_TABS = "file";
-      UPLOADCARE_PUBLIC_KEY = "your_public_key";
-      // Uploadcare script start
-      $ = uploadcare.jQuery;
-      // Create uploaded image list and append additional form fields to each item
-      
-      function installWidgetPreviewMultiple(widget, list) {
-        widget.onChange(function(fileGroup) {
-
-          var groupPromise = fileGroup.promise();
-          groupPromise.done(function(fileGroupInfo) {
-          // Upload successfully completed and all files in the group are ready.
-          });
-          list.empty();
-          if (fileGroup) {
-            $.when.apply(null, fileGroup.files()).done(function() {
-
-              $.each(arguments, function(i, fileInfo) {
-                
-                // display file preview
-                var $filename = fileInfo.name;// display file name
-                var $fileurl = fileInfo.cdnUrl;// get file url
-                var $src = fileInfo.cdnUrl + '-/resize/100x100/filename.jpg';// preview image source, resize to 100X100px and jpeg file type
-                // append preview and name and form fields to each file uploaded inside thumb_list 
-                list.append(
-                  $('<li class="thumb_list_item"><img src="' + $src+ '" alt="File Preview" class="preview-img">' + '<h4 class="filename">' + $filename + '</h4></li>').appendTo(".thumb_list")
-                  );
-              });
-            });
-          }
-        });
-      }
-    $(function() {
-      $('.upload-area').each(function() {
-        installWidgetPreviewMultiple(
-          uploadcare.MultipleWidget($(this).children('input')),
-          $(this).children('.thumb_list')
-        );
-      });
-    });
-    $( "#submit" ).click(function() {
-      $( ".thumb_list" ).empty();
-    });
-    
-    
-
-    </script>
-   
-
-<link rel="stylesheet" type="text/css" href="<?php echo plugin_dir_url(__FILE__);?>/trial-styles.css">
-<!-- <link rel="stylesheet" type="text/css" href="<?php echo plugin_dir_url(__FILE__);?>/GeoLite2-Country.mmdb"> -->
 <?php
+
 $token = uniqid();
-$output .='<div class="trial-form-wraper">     
-            <div class="form-body">
-                <form method="POST" action="'.plugin_dir_url(__FILE__).'mailer.php'.'" id="uc_form">
-                <input type="hidden" name="token" value="'.$token.'">
-                 <ul class="trial-form">
-    <li class="trial-form-item trial-form-item-sm">
-      <label class="trial-form-lbl">Full Name <span class="trial-must">*</span></label>
-      <input type="text" name="fname" id="fname" class="trial-form-input" placeholder="Enter your full name" required>
-    </li>
-    </li>
-    <li class="trial-form-item trial-form-item-sm">
-      <label class="trial-form-lbl">Email <span class="trial-must">*</span></label>
-      <input type="email" name="email" id="email" class="trial-form-input" placeholder="Enter your email address" required>
-    </li>
-    <li class="trial-form-item trial-form-item-sm">
-      <label class="trial-form-lbl">Telephone </label>
-      <input type="tel" name="phone" id="telephone" class="trial-form-input" placeholder="Enter your contact number">
-    </li>
-    <li class="trial-form-item trial-form-item-sm">
-      <label class="trial-form-lbl">Country </label>
-      <select class="trial-form-input" id="client_country" name="client_country">
-              </select>
-    </li>
-    <li class="trial-form-item trial-form-item-full">
-      <label class="trial-form-lbl">Brief Instruction <span class="trial-must">*</span></label>
-      <textarea id="description" name="description" class="trial-form-txtbox trial-form-input" placeholder="Briefly write your instructions for this free order" required></textarea>
-    </li>
-    <li class="trial-form-item trial-form-item-full">
-      <label class="trial-form-lbl">Upload files/images here (Maximum: 3). <span class="trial-must">*</span></label>
-      <input type="hidden" id="uploader" name="uploader" role="uploadcare-uploader" data-multiple="true" data-multiple-max="3" title="Click here to upload your file or Drag and drop your files" required>
+$output = null;
 
-    </li>
-    
-    <li class="trial-form-item">
-      <input type="submit" id="submit" class="submit-trial" value="Send My Free Trial Order"><!-- submit form -->
-    </li>
-  </ul>
+$myipd = get_client_ip(); 
+$url = 'http://www.geoplugin.net/json.gp?ip='.$myipd; 
+$details    =   ip_details($url); 
+$v = json_decode($details);
+$mycountry = $v->geoplugin_countryName;
+$mycode = $v->geoplugin_countryCode;
 
-                  <ul class="thumb_list">
-                      
-                  </ul>                   
-                </form>
-            </div>
-    </div> ';
-
+function countrySelector($defaultCountry =""){
+    global $countryArray; // Assuming the array is placed above this function
+	$telcode=$country["code"];
+	foreach($countryArray as $code => $country){
+		if ( $code==strtoupper($defaultCountry) ){
+		$countryName = ucwords(strtolower($country["name"])); // Making it look good
+		$output .= "<input type='text' id='tel-code' name='tel-code' class='input-part tel-code' ".(($code==strtoupper($defaultCountry))?"value='(".$code.") +".$country["code"]."' ":"")."readonly>";
+	}
+	}
+	return $output; // or echo $output; to print directly
+}
+$output .='
+<form id="uc_form" action="'.plugin_dir_url(__FILE__).'mailer.php'.'" method="post" enctype="multipart/form-data">
+<input type="hidden" name="token" value="'.$token.'">
+<input type="hidden" name="country" value="'.$mycountry.'">
+	<div class="my-trial">
+		<div class="sec-trial">
+			<a href="#" class="close-btn"> X</a>
+			<div class="form-head">
+				<p class="form-title">Try Us Free</p>
+				<p class="form-txt">Let&apos;s upload maximum <strong> 3 images/files </strong>here to submit the trial.  </p>
+				<div id="ie-uploader" class="upload-area"><input type="hidden" id="uploader" name="uploader" role="uploadcare-uploader" data-multiple="true" data-system-dialog="true" data-multiple-max="3" title="Click here to upload your file or Drag and drop your files" required></div>
+			</div>
+			<div class="form-show pop-trial">
+				<p class="form-title">Upload Done!</p>
+				<p class="form-txt">Now fill this form to complete your Free Trial Request.  </p>
+				<ul>
+					<li class="trial-form-item">
+					<label class="trial-form-lbl" for="fname">Full Name <span class="trial-must">*</span></label>
+					<input type="text" name="fname" id="fname" class="trial-form-input" placeholder="Enter your full name" required>
+					</li>
+					</li>
+					<li class="trial-form-item">
+					<label class="trial-form-lbl" for="email">Email <span class="trial-must">*</span></label>
+					<input type="email" name="email" id="email" class="trial-form-input" placeholder="Enter your email address" required>
+					</li>
+					<li class="trial-form-item">
+					<label class="trial-form-lbl" for="phone">Telephone <span class="trial-must">*</span></label>
+					<div class="trial-form-input no-pad">'.countrySelector($mycode).'
+					<input type="tel" name="phone" id="telephone" class="input-part tel-input" placeholder="Enter your contact number" required></div>
+					</li>
+					<div class="clear"></div>
+					<li class="trial-form-item trial-form-item-full">
+					<label class="trial-form-lbl" for="description">Brief Instruction <span class="trial-must">*</span></label>
+					<textarea id="description" name="description" class="trial-form-txtbox trial-form-input" pattern="[a-zA-Z0-9-]" placeholder="Enter brief instruction here" required></textarea>
+					</li>
+					<li class="trial-form-item trial-form-item-full">
+					<div class="g-recaptcha" data-sitekey="google recaptcha site key"></div></li>
+					<li>
+					<input type="submit" id="bow" class="bow-trial" value="Send My Free Trial Order">
+					<p>By submitting this Free Trial you agree to our <a href="https://example.com/terms-of-use/" target="_blank"><span class="theme-txt-color">Terms of Use</span></a>.</span> </a> </p>
+					</li>
+				</ul>
+				<ul class="thumb_list"></ul>
+			</div>
+		</div>
+	</div>
+</form>';
 return $output;
 }
 add_shortcode('trial_form', 'trial_form');
