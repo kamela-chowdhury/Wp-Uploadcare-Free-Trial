c2d2f897352a78432340ef4395947c1717430b04
Suparna Chowdhury <kamela@metrodesk.com.bd>
2018-01-17 11:47:30 +0600

    New updates

--- mailer.php
+++ mailer.php
@@ -1,45 +1,59 @@
 <?php 
 require 'vendor/autoload.php';
-use \Uploadcare;
 
-$sendgrid = new SendGrid('sendgrid api key');// sendgrid api key
-$api = new Uploadcare\Api('uploadcare public key', 'uploadcare secret key');// uploadcare api key
+$sendgrid = new SendGrid('Sendgrid API key');
+$api = new Uploadcare\Api('Uploadcare public key', 'Uploadcare secret key');
 
 $mail = new SendGrid\Email();// send email to ImageEditing
 $mail2 = new SendGrid\Email();// send confirmation email to client
 
 // Sender details
-$name = "Sender name";// Sender name
+$token = $_POST['token'];//get token to create Order Id
+// Sender details
+$name = "example";// Sender name
 $fname = $_POST['fname'];// Sender Name
 $email = $_POST['email'];// Sender Email
 $phone = $_POST['phone'];// Sender Telephone
-$client_country = $_POST['client_country'];// Sender Country
 $description = $_POST['description'];// Sender Message
-
 $fileurl = $_POST['uploader'];//get files URL
-$token = $_POST['token'];//get token to create Order Id
-
+$country = $_POST['country'];// Sender country
+$telcode = $_POST['tel-code'];
 $group = $api->getGroup($fileurl);
 $group->store();
 $files = $group->getFiles();
 
 $total = count($files);
-$msg = "Free Trial Id: $token \nDear Super Admin\nA new free trial has been submitted on example.com.\n\nName: $fname\nEmail Address: $email\nTelephone: $phone \nCountry: $client_country.\n\nBrief: $description\n\nAll files download Link: $fileurl \n\n\n";
 
+// recaptcha.
+if(isset($_POST['g-recaptcha-response'])){
+       $response=$_POST['g-recaptcha-response'];
+     }else{echo 'not posted';}
+$response = $_POST["g-recaptcha-response"];
+	if(!$response){
+          echo '<h2>Please check the captcha form.</h2>';
+          exit;
+        }
+        $secretKey = "Google Recaptcha Key";
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
+        $responseKeys = json_decode($response,true);
+        if(intval($responseKeys["success"]) !== 1) {
+          echo '<h2>Uh oh, spammer alert. This is no place for a bot. Please try again as a human.</h2>';
+        } 
+$msg = "Free Trial Id: $token \r\n\r\nDear Admin,\r\n\r\nA new free trial has been submitted on example.\r\n\r\nName: $fname\r\n\r\nEmail: $email\r\n\r\nCountry: $country\r\n\r\nTelephone: $telcode\r\n\r\nEmail Address: $email\r\n\r\nTelephone: $phone \r\n\r\n\r\nBrief Instruction: $description\r\n\r\n";
 
-for($i = 0; $i < count($files); $i++) {
-    $msg.= "Download Link: ".$files[$i]->getUrl()."\n";
+	for($i = 0; $i < count($files); $i++) {
+    $msg.= "Download Link: ".$files[$i]->getUrl()."\r\n\r\n";
     }
-$msg .= "\n\nKind Regards \example.com \nSkype: example[24 hours] \nEmail:support@example.com";
-$recipient ="example@example.com";
-$subject = "$token - New Free Trial from ImageEditing";
-// Confirmation reciever
-$confirm_subject = "Free trial order submitted";
-$confirm_msg = "Dear $fname,\nThank you for trying ImageEditing.Com free trial service. Your free trial Id: $token. \n\nWe will notify you when your order is ready.\n\nFor any support you can reply to this email or call us at 123456789.\n\nKind Regards \example \nVisit: www.example.com \nSkype: example[24 hours] \nEmail: support@example.com";
 
-// mail($recipient, $subject, $from, $mailheader) or die("Error!");
+	$msg .= "You can download all files from this group link: $fileurl \r\n\r\nPlease review the instruction carefully and process the trial order accordingly.\r\n\r\nKind Regards, \r\n\r\ \r\n\r\nSkype: examplebd[24 hours] \r\n\r\nEmail: support@example.com";
+	$recipient ="support@example.com";
+	$subject = "$token - New Free Trial from example";
+	// Confirmation reciever
+	$confirm_subject = "Thank you for trying example";
+	$confirm_msg = "Dear $fname,\r\n\r\nThank you for trying example Free Trial service. Your free trial Id: $token. \r\n\r\nWe will notify you when your order is ready.\r\n\r\nFor any support you can reply to this email or call us at 123456789.\r\n\r\nKind Regards, \r\n\r\example \r\n\r\nVisit: www.example.com\r\n\r\nEmail: support@example.com";
 
-
+	// mail($recipient, $subject, $from, $mailheader) or die("Error!");
 	$mail->
 	addTo( $recipient )->
 	setFromName($name)->
@@ -55,13 +69,11 @@ $confirm_msg = "Dear $fname,\nThank you for trying ImageEditing.Com free trial s
 	setText($confirm_msg);
   	//Send Mail.
 	if ( ($sendgrid->send($mail)) && ($sendgrid->send($mail2)) ){
-		exit(header('Location: http://example.com/thank-you/'));
+		exit(header('Location: https://example.com/thankyou/'));
 	}
 	else{
-		echo "Order submission failed. Please check for error or report to support@example.com for Support.";
+		echo "Order submission failed. Please check for error or report to support@example.com for Support. For more, visit example.com";
 	}
-
-
 ?>
 
 
